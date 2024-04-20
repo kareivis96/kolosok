c6b8931d78d099e17c10a8301bcfdc919c16897e
Klyuchnikov <96Dimian@inbox.ru>
2024-04-20 18:22:47 +0300

    add sidebar links

---
+++ src/components/Sidebar/components/SidebarContent/SidebarContent.tsx
@@ -0,0 +1,23 @@
+import type { FC } from 'react';
+import { memo } from 'react';
+
+import { APP_LINKS } from 'constants/routes';
+
+import { SidebarLink } from '../SidebarLink';
+import CSS from './SidebarContent.module.scss';
+
+const SidebarContent: FC = () => {
+    const { menu, students, attendance, schedule, teachers } = APP_LINKS;
+
+    return (
+        <div className={CSS.sidebar_content}>
+            <SidebarLink title={students.title} link={students.link} isDiabled={students.isDisabled} />
+            <SidebarLink title={menu.title} link={menu.link} isDiabled={menu.isDisabled} />
+            <SidebarLink title={attendance.title} link={attendance.link} isDiabled={attendance.isDisabled} />
+            <SidebarLink title={schedule.title} link={schedule.link} isDiabled={schedule.isDisabled} />
+            <SidebarLink title={teachers.title} link={teachers.link} isDiabled={teachers.isDisabled} />
+        </div>
+    );
+};
+
+export default memo(SidebarContent);
--- src/types/routes/index.ts
+++ src/types/routes/index.ts
@@ -1,6 +1,10 @@
 export enum EAppLinks {
     Home = 'home',
     Menu = 'menu',
+    Students = 'students',
+    Attendance = 'attendance',
+    Schedule = 'schedule',
+    Teachers = 'teachers',
 }
 
 export type TAppLinks = {
