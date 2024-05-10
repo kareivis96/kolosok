dc4ac346999aab6595f73e3fd0c272d7f792e902
Klyuchnikov <96Dimian@inbox.ru>
2024-05-10 16:11:48 +0300

    feature/gh_pages настройки CI

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: process.env.MODE === 'prod' ? '/kolosok/' : '/',
+    base: '/kolosok/',
     plugins: [react()],
     resolve: {
         alias: {
