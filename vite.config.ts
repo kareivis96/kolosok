e07ef7cee26f6eb0fc4dd60bcb27ce7184dc3a12
Klyuchnikov <96Dimian@inbox.ru>
2024-05-10 15:59:43 +0300

    feature/gh_pages настройки CI

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/kolosok/',
+    base: '/kolosok',
     plugins: [react()],
     resolve: {
         alias: {
